2e4e8dbd9126f469d5778240cbb3b9c767371054
greyna <arthur.van-ceulen@greyna.eu>
2015-01-20 10:48:37 +0100

    start.sh modified

--- spreadit-server/start.sh
+++ spreadit-server/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup java -jar spreadit-server-CloudSQL-1.2.jar > spreadit-log.txt 2> spreadit-log.txt < /dev/null &
+nohup java -jar spreadit-server-CloudSQL-1.3.jar > spreadit-log.txt 2> spreadit-log.txt < /dev/null &
 PID=$!
 echo $PID > pid.txt
