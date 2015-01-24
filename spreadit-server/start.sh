9a390bd3d4e3fbe54135b8c4181311c9ef026ce6
greyna <arthur.van-ceulen@greyna.eu>
2015-01-24 21:14:13 +0100

    server port modified to 8181

--- spreadit-server/start.sh
+++ spreadit-server/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup java -jar spreadit-server-CloudSQL-1.4.jar > spreadit-log.txt 2> spreadit-log.txt < /dev/null &
+nohup java -jar spreadit-server-CloudSQL-1.4.jar --server.port=8181 > spreadit-log.txt 2> spreadit-log.txt < /dev/null &
 PID=$!
 echo $PID > pid.txt
